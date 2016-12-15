a7dc792e2c836cd694672ce32a5556f8ee164132
barbudos <45ter01d>
2016-12-15 16:13:55 +0300

    oops

--- src/com/javarush/test/level30/lesson06/task01/Solution.java
+++ src/com/javarush/test/level30/lesson06/task01/Solution.java
@@ -14,9 +14,13 @@ public class Solution {
     }
 
     public static void checkAFlag(D d) {
-        if(d != null && d.cs.get(0) != null && d.cs.get(0).bs.get(0) != null && d.cs.get(0).bs.get(0).as.get(0) != null && d.cs.get(0).bs.get(0).as.get(0).flag){
-            System.out.println("A's flag is true");
-        } else { //all other cases
+        try {
+            if(d.cs.get(0).bs.get(0).as.get(0).flag){
+                System.out.println("A's flag is true");
+            } else { //all other cases
+                System.out.println("Oops!");
+            }
+        } catch (Exception e) {
             System.out.println("Oops!");
         }
     }
